d0af406d225d1cf96bbfea168619d4750a7be432
leenorris <lee.norris@philips.com>
2019-04-14 11:22:33 -0400

    first commit

---
+++ src/packaging/BinaryCleaner.ts
@@ -0,0 +1,32 @@
+import {execSync} from 'child_process';
+import {parse} from 'path';
+import * as _ from 'lodash';
+
+export class BinaryCleaner {
+
+  static deleteBinaryLink (command: string, execSyncOverride?: (cmd: string) => Buffer) {
+    const bash = execSyncOverride || execSync;
+    const dir = BinaryCleaner.getNodeBin();
+    dir && bash((`rm -rf ${dir}/${command}`));
+  }
+
+  static getNodeBin (execSyncOverride?: (cmd: string) => Buffer): string {
+    const bash = execSyncOverride || execSync;
+
+    try {
+      const filePathToTest = bash(`which npm`)
+        .toString()
+        .split('\n')
+        .filter(i => i && i.length)[0];
+
+      const {dir} = parse(filePathToTest);
+
+      console.log(dir);
+
+      return dir;
+    }
+    catch (e) {
+      return '';
+    }
+  }
+}
---
+++ src/packaging/BinaryPackager.ts
@@ -0,0 +1,76 @@
+import {execSync} from 'child_process';
+import {readdirSync, writeFileSync, readFileSync} from 'fs';
+import * as _ from 'lodash';
+import {BinaryCleaner} from './BinaryCleaner';
+
+export class BinaryPackager {
+  emitTranspiledSubDir = false;
+
+  static buildWithDefaultsAndInjection (cmd: string, projectFolderName: string,
+    execSyncOverride: (command: string) => Buffer,
+    readdirSyncOverride: (path: string) => string[],
+    writeFileSyncOverride: (path: string | number, data: any, options?: any) => void,
+    readFileSyncOverride: (path: string) => any) {
+    return new BinaryPackager(cmd, projectFolderName, undefined, undefined, undefined, undefined, execSyncOverride, readdirSyncOverride, writeFileSyncOverride, readFileSyncOverride);
+  }
+
+  static buildWithDefaults (cmd: string, projectFolderName: string) {
+    return BinaryPackager.buildWithDefaultsAndInjection(cmd, projectFolderName, execSync, readdirSync, writeFileSync, readFileSync);
+  }
+
+  static build (
+    cmd: string,
+    projectFolderName: string,
+    packagePath: string,
+    srcDir: string,
+    transpiledDir: string,
+    binDir: string,
+    execSyncOverride: (command: string) => Buffer,
+    readdirSyncOverride: (path: string) => string[],
+    writeFileSyncOverride: (path: string | number, data: any, options?: any) => void,
+    readFileSyncOverride: (path: string) => any
+  ) {
+    return new BinaryPackager(cmd, projectFolderName, packagePath, srcDir, undefined, undefined, execSyncOverride, readdirSyncOverride, writeFileSyncOverride, readFileSyncOverride);
+  }
+
+  constructor (private cmd: string,
+    private projectFolderName: string,
+    private packagePath = 'package.json',
+    private srcDir = 'cli',
+    private transpiledDir = 'transpiled',
+    private binDir = '.binary',
+    private execSyncOverride: (command: string) => Buffer,
+    private readdirSyncOverride: (path: string) => string[],
+    private writeFileSyncOverride: (path: string | number, data: any, options?: any) => void,
+    private readFileSyncOverride: (path: string) => any
+  ) {
+    const rawPackageContents = (this.readFileSyncOverride || readFileSync)(this.packagePath);
+    const packageContents = JSON.parse(rawPackageContents);
+
+    const cliWrapperFiles = (this.readdirSyncOverride || readdirSync)(this.srcDir)
+      .map(this.toScriptObj());
+
+    (this.execSyncOverride || execSync)(`rm -rf ${this.binDir} && mkdir ${this.binDir}`);
+
+    cliWrapperFiles
+      .forEach((script: any) => {
+        console.log(`adding script to package.json "bin" section: ${script.cmdName} => ${script.scriptPath}`);
+        packageContents.bin[script.cmdName] = script.transpiledPath;
+        BinaryCleaner.deleteBinaryLink(script.cmdName, undefined);
+      });
+
+    (this.writeFileSyncOverride || writeFileSync)(this.packagePath, JSON.stringify(packageContents, null, 4), 'utf8');
+
+  }
+
+  toScriptObj () {
+    return (name: string) => {
+      const transpiledSrc = this.emitTranspiledSubDir ? `${this.projectFolderName}/` : '';
+      const scriptPath = `${transpiledSrc}${this.srcDir}/${name}`;
+      const cmdName = this.cmd + '-' + name.replace('.js', '').replace('.ts', '');
+      const transpiledPath = `${this.transpiledDir}/${scriptPath}`.replace('.ts', '.js');
+      return _.assign({dir: this.srcDir, name, scriptPath, cmdName, transpiledPath});
+    };
+  }
+
+}
