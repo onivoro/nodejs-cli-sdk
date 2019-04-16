b0984ab49b830905ce6506f0acd45c3e3a606442
leenorris <lee.norris@philips.com>
2019-04-16 05:57:49 -0400

    transpiled

--- transpiled/cli/generate-project.js
+++ transpiled/cli/generate-project.js
@@ -2,9 +2,5 @@
 "use strict";
 exports.__esModule = true;
 var child_process_1 = require("child_process");
-var lodash_1 = require("lodash");
 child_process_1.execSync("git clone https://github.com/onivoro/nodejs-cli-starter.git --depth 1 --no-tags");
-[].forEach(function (term) {
-    child_process_1.execSync("git grep -w " + term + " --name-only").toString().split('\n')
-        .filter(lodash_1.identity);
-});
+child_process_1.execSync("rm -rf .git && git init");
